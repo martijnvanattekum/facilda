2baee78249085b5d9690afbe6dbb3e8c87b2267e
Van_Attekum,Dr.,Martijn (GCBDS) BIP-DE-B <martijn.van_attekum@boehringer-ingelheim.com>
2023-07-21 12:01:08 +0200

    added automap_ids function

--- R/compbio.R
+++ R/compbio.R
@@ -1,21 +1,37 @@
-#
-# #input: vector of same-type IDs, function will detect the type and map to
-# #return_type using the map Ids function of org.Hs.eg.db
-# automapIDs <- function(IDs, return_type = "SYMBOL") {
-#   all_keys <- sapply(keytypes(org.Hs.eg.db), function(keytype) keys(org.Hs.eg.db, keytype), USE.NAMES = TRUE)
-#
-#   IDs <- as.character(IDs)
-#   IDs_copy <- IDs
-#
-#   #remove potential trailing .x version number from ensembl and refseq IDs
-#   IDs <- gsub("((^ENS[GPT])[^\\.]*)\\.[0-9]+", "\\1", IDs)
-#   IDs <- gsub("((^[NXY][MPR]_)[^\\.]*)\\.[0-9]+", "\\1", IDs)
-#
-#   #determine which keytype (search key) was provided by user
-#   IDs_in_keys <- sapply(all_keys, function(keys) mean(IDs %in% keys)) #occurence of IDs in key sets
-#   if (all(IDs_in_keys < .2)) stop(paste0("Type of IDs not recognized. Valid types are: ", paste(names(all_keys), collapse = ", ")))
-#   keytype <- names(all_keys)[which.max(IDs_in_keys)]  #determine the most likely provided type
-#   out <- mapIds(org.Hs.eg.db, keys = IDs, column = return_type, keytype = keytype, multiVals = "first")
-#   names(out) <- IDs_copy  #restore original names
-#   out
-# }
+#' Detects annotation identifiers and converts them to the requested format
+#'
+#' Uses the org.Hs.eg.db database to detect the keytype of the input vector
+#' and converts it to the keytype defined by return_keytype.
+#' When multiple target ids are found for a source id, the first will be
+#' returned.
+#'
+#' @param ids vector containing the original ids, all of the same keytype
+#' @param return_keytype format which to convert ids to. Options can be found
+#' by running library(org.Hs.eg.db); keytypes(org.Hs.eg.db)
+#' @return named vector containing the converted ids, named with the original
+#' ids
+#' @examples
+#' ensembl_ids <- c("ENSG00000132475.8", "ENSG00000185339.8")
+#' symbols <- automap_ids(ensembl_ids)
+
+#' @import org.Hs.eg.db purrr AnnotationDbi
+#' @export
+automap_ids <- function(ids, return_keytype = "SYMBOL") {
+
+  db_ids_by_type <- purrr::map(keytypes(org.Hs.eg.db) %>% purrr::set_names(), ~AnnotationDbi::keys(org.Hs.eg.db, .x))
+
+  original_ids <- ids
+  ids_for_conversion <- ids %>%
+    as.character() %>%
+    # potential trailing .n version number from ensembl and refseq ids must be removed before conversion
+    stringr::str_replace("((^ENS[GPT])[^\\.]*)\\.[0-9]+", "\\1") %>%
+    stringr::str_replace("((^[NXY][MPR]_)[^\\.]*)\\.[0-9]+", "\\1")
+
+  perc_ids_found_per_keytype <- purrr::map_dbl(db_ids_by_type, ~mean(ids_for_conversion %in% .x))
+  if (all(perc_ids_found_per_keytype < .2)) stop(paste0("Type of ids not recognized. Valid types are: ", paste(names(db_ids_by_type), collapse = ", ")))
+  detected_keytype <- names(perc_ids_found_per_keytype)[which.max(perc_ids_found_per_keytype)]
+
+  AnnotationDbi::mapIds(org.Hs.eg.db, keys = ids_for_conversion, column = return_keytype, keytype = detected_keytype, multiVals = "first") %>%
+    set_names(original_ids)
+
+  }
